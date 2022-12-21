99fa87f25d280ec1cee5a783deccc223d0f5cc1d
Katerina Gezerli <katerinagezerli@yahoo.gr>
2022-12-21 19:22:31 +0200

    Add print statement warning regarding Washington's lack of user data  in the end of the code

--- bikeshare.py
+++ bikeshare.py
@@ -6,6 +6,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
+
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
@@ -204,6 +205,7 @@ def main():
         if restart.lower() != 'yes':
             break
 
-
 if __name__ == "__main__":
 	main()
+
+print('Lack of user data for Washington city')
